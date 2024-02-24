de03634aefcf24ea7d723cf75e3ecbb4a1574e69
elipappas <eli@bpi.net>
2024-02-24 18:43:31 -0500

    updated styles

--- Medical-Exam-Site/src/app/layout.js
+++ Medical-Exam-Site/src/app/layout.js
@@ -4,10 +4,10 @@ import Link from 'next/link'
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body>
+        <body className="bg-slate-700 text-white">
           <main>
             <nav>
-              <ul className="flex space-x-4"> 
+              <ul className="flex space-x-4 justify-center bg-slate-800 p-2"> 
                 <Link href="/"><li className="mb-4 hover:underline underline-offset-4 text-left w-44 inline"> Home </li></Link> 
                 <Link href="/login"><li className="mb-4 hover:underline underline-offset-4 text-left w-44 inline"> Login </li></Link> 
                 <Link href="/medical_form"><li className="mb-4 hover:underline underline-offset-4 text-left w-44 inline"> Medical Form </li></Link> 
--- Medical-Exam-Site/src/app/login/page.js
+++ Medical-Exam-Site/src/app/login/page.js
@@ -1,6 +1,6 @@
 export default function login() {
   return (
-    <div class="flex p-4 pl-16 w-full items-center bg-zinc-950">
+    <div class="">
       <p> login page </p>
     </div>
   );
