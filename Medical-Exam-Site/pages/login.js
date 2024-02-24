59664254857bc56c444cd28fb6e9aba0d9dca149
Grant Guernsey <guernsgd@mail.uc.edu>
2024-02-24 15:37:45 -0500

    Fix

--- Medical-Exam-Site/pages/login.js
+++ Medical-Exam-Site/pages/login.js
@@ -1,6 +1,5 @@
 import Navbar from '../comps/navbar'
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
-import { useState } from 'react'; // Import useState hook
 
 export default function login() {
   return (
