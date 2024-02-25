a577bd80310893f03f0ecaabf6f9e0738b170300
elipappas <eli@bpi.net>
2024-02-25 10:08:18 -0500

    i really hate zeb

--- Medical-Exam-Site/src/app/medical_form/page.js
+++ Medical-Exam-Site/src/app/medical_form/page.js
@@ -9,7 +9,20 @@ import { useState } from 'react';
 //   return accessToken;
 // });
 
-// export { GET };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://10.11.191.67:3000/symptomList');
+        if (response.status > 400) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        // Extracting names from the fetched data and updating state
+        setSymptoms(data.map(symptom => symptom.name));
+      } catch (error) {
+        setError(error.message || 'Error fetching data');
+      }
+    };
 
 // async function createHeader() {
 //   let x = await GET();
@@ -18,7 +31,7 @@ import { useState } from 'react';
 // }
 
 // /status
-export default async function MedicalForm() {
+async function MedicalForm() {
   //let headers = await createHeader();
   //let retval = await fetch("http://10.11.191.67:3001/status");
   const [ symptomList, setSymptomList ] = useState( [] )
@@ -29,22 +42,37 @@ export default async function MedicalForm() {
 
   return (
     <div className="flex justify-center pt-8 pb-8">
-      <form className="w-1/2 bg-slate-800 pl-14 pr-14 pt-4 rounded-xl">
-        <label htmlFor="symptoms">Symptoms: </label>
-        <br></br>
-        <textarea name="symptoms" rows="10" cols="25" className="text-black rounded-xl"></textarea>
-        <br></br>
-        <label htmlFor="notes">Additional Notes: </label>
-        <br></br>
-        <textarea name="notes" rows="10" cols="25" className="text-black rounded-xl"></textarea>
-        <br></br>
-        <label htmlFor="file">Upload Files Here: </label>
-        <br></br>
+      <form className="w-1/2 bg-slate-700 pl-14 pr-14 pt-4 rounded-xl">
+        <br />
+        <details className="text-black bg-white p-1 w-72">
+          <summary>Choose Symptoms</summary>
+          {/* Render checkboxes for each symptom */}
+          {symptoms.map((symptom, index) => (
+            <div key={index}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={symptom}
+                  onChange={handleCheckboxChange}
+                  checked={selectedSymptoms.includes(symptom)}
+                />
+                {symptom}
+              </label>
+            </div>
+          ))}
+        </details>
+        <br />
+        <label htmlFor="notes">Additional Notes:</label>
+        <br />
+        <textarea name="notes" rows="10" className="text-black w-72"></textarea>
+        <br/>
+        <label htmlFor="file">Upload Files Here:</label>
+        <br/>
         <input id="file" name="file" type="file" />
-        <br></br>
-        <br></br>
-        <input type="submit" className="bg-white p-2 text-black rounded-xl" />
+        <br/>
+        <br/>
+        <input type="submit" className="bg-white p-1 text-black" />
       </form>
     </div>
   );
-}
+}})
