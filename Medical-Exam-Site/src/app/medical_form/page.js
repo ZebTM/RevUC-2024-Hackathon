5c66298451eece3cb0d43ede507a113ad3f54e3c
elipappas <eli@bpi.net>
2024-02-25 09:55:35 -0500

    i hate zeb

--- Medical-Exam-Site/src/app/medical_form/page.js
+++ Medical-Exam-Site/src/app/medical_form/page.js
@@ -1,70 +1,37 @@
-"use client"
-import React, { useState, useEffect } from 'react';
+// import { NextResponse } from 'next/server';
+// import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
-export default function MedicalForm() {
-  const [symptoms, setSymptoms] = useState([]);
-  const [selectedSymptoms, setSelectedSymptoms] = useState([]);
-  const [error, setError] = useState(null);
+// const GET = withApiAuthRequired(async function GET(req) {
+//   const res = new NextResponse();
+//   const { accessToken } = await getAccessToken(req, res);
+//   return accessToken;
+// });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://10.11.191.67:3001/symptoms');
-        if (response.status > 400) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        // Extracting names from the fetched data and updating state
-        setSymptoms(data.map(symptom => symptom.symptoms));
-      } catch (error) {
-        setError(error.message || 'Error fetching data');
-      }
-    };
+// export { GET };
 
-    fetchData();
-  }, []);
+// async function createHeader() {
+//   let x = await GET();
+//   const header = {'Header': `Bearer ${x}`};
+//   return header;
+// }
 
-  const handleCheckboxChange = (event) => {
-    const { value, checked } = event.target;
-    if (checked) {
-      setSelectedSymptoms([...selectedSymptoms, value]);
-    } else {
-      setSelectedSymptoms(selectedSymptoms.filter(symptom => symptom !== value));
-    }
-  };
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
+// /status
+export default async function MedicalForm() {
+  //let headers = await createHeader();
+  //let retval = await fetch("http://10.11.191.67:3001/status");
   return (
     <div className="flex justify-center pt-8 pb-8">
-      <form className="w-1/2 bg-slate-700 pl-14 pr-14 pt-4">
-        <br />
-        <details className='bg-white p-2 text-black w-72'>
-          <summary>Choose Symptoms</summary>
-          {/* Render checkboxes for each symptom */}
-          {symptoms.map((symptom, index) => (
-            <div key={index}>
-              <label>
-                <input
-                  type="checkbox"
-                  value={symptom}
-                  onChange={handleCheckboxChange}
-                  checked={selectedSymptoms.includes(symptom)}
-                />
-                {symptom}
-              </label>
-            </div>
-          ))}
-        </details>
-        <br />
-        <label htmlFor="notes">Additional Notes:</label>
-        <br />
-        <textarea name="notes" rows="10" className="text-black w-72"></textarea>
-        <br/>
-        <label htmlFor="file">Upload Files Here:</label>
-        <br/>
+      <form className="w-1/2 bg-slate-800 pl-14 pr-14 pt-4 rounded-xl">
+        <label htmlFor="symptoms">Symptoms: </label>
+        <br></br>
+        <textarea name="symptoms" rows="10" cols="25" className="text-black rounded-xl"></textarea>
+        <br></br>
+        <label htmlFor="notes">Additional Notes: </label>
+        <br></br>
+        <textarea name="notes" rows="10" cols="25" className="text-black rounded-xl"></textarea>
+        <br></br>
+        <label htmlFor="file">Upload Files Here: </label>
+        <br></br>
         <input id="file" name="file" type="file" />
         <br/>
         <br/>
